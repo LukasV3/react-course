c276f14f3f109c18eef1078dd948a235a4a3e7bb
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-01-12 15:13:35 +0000

    created ApprovalCard and nested CommentDetail

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          comment="Nice blog post!"
+          image={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          comment="BORING!"
+          image={faker.image.image()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          comment="You're funny"
+          image={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
