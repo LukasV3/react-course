0acd51c6a3855d9c9556cd6f852d19718d55497d
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-01-19 18:21:42 +0000

    completed Accordion component and created Search component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a frontend framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React? ",
+    content: "You use React by creating components",
+  },
+];
 
 export default () => {
-  return <div>Widgets App</div>;
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
