1f218fee0b81826f65fc5b6fd842205febb3701f
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-01-19 13:19:01 +0000

    created VideList and VideoItem componenets along with an event handler calling a callback func passed down from parent App via props system

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  onTermSubmit = async (term) => {
+    const res = await youtube.get("./search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: res.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     );
   }
