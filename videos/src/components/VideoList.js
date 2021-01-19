f454ee7bb5d4549a65be99db6058d5daa67830c3
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-01-19 13:52:21 +0000

    created VideoDetail component and implemented video player with default selection

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -3,7 +3,9 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const redneredList = videos.map((video) => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+    return (
+      <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />
+    );
   });
 
   return <div className="ui relaxed divided list">{redneredList}</div>;
