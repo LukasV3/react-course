95a9d63c9c592f4f4e06fd979296accf27a804ec
LukasV3 <volk.lukas237@yahoo.co.uk>
2021-01-30 10:49:57 +0000

    finished implementing LanguageStore and connecting nested components to the store

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -5,17 +5,12 @@ import ColorContext from "../contexts/ColorContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
         <LanguageStore>
-          <LanguageSelector onLanguageChange={this.onLanguageChange} />
+          <LanguageSelector />
+
           <ColorContext.Provider value="red">
             <UserCreate />
           </ColorContext.Provider>
